8e36df6b2468cdc7aea3ea4adef93c1fdc077911
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2026-01-31 19:36:39 -0300

    Criar colapso no widget ao pressionar o botão maximizar/minimizar

--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -1,18 +1,29 @@
+import * as Collapsible from "@radix-ui/react-collapsible";
 import { UploadWidgetDropzone } from "./upload-widget-dropzone";
 import { UploadWidgetHeader } from "./upload-widget-header";
 import { UploadWidgetUploadList } from "./upload-widget-upload-list";
+import { useState } from "react";
+import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button";
 
 export function UploadWidget() {
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+
   return (
-    <div className="bg-zinc-900 w-full overflow-hidden max-w-90 rounded-xl shadow-shape">
-      <UploadWidgetHeader />
-      <div className="flex flex-col gap-4 py-3">
-        <UploadWidgetDropzone />
+    <Collapsible.Root onOpenChange={setIsWidgetOpen}>
+      <div className="bg-zinc-900 overflow-hidden w-90 rounded-xl shadow-shape">
+        {!isWidgetOpen && <UploadWidgetMinimizedButton />}
 
-        <div className="h-px bg-zinc-800 border-t border-black/50 box-content"></div>
+        <Collapsible.Content>
+          <UploadWidgetHeader />
+          <div className="flex flex-col gap-4 py-3">
+            <UploadWidgetDropzone />
 
-        <UploadWidgetUploadList />
+            <div className="h-px bg-zinc-800 border-t border-black/50 box-content"></div>
+
+            <UploadWidgetUploadList />
+          </div>
+        </Collapsible.Content>
       </div>
-    </div>
+    </Collapsible.Root>
   );
 }
