9cd04cec13bdeb68d7b688780b5adc1dfd6043ae
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2026-02-07 00:22:59 -0300

    Criar animações no Widget

--- src/components/upload-widget-upload-item.tsx
+++ src/components/upload-widget-upload-item.tsx
@@ -1,10 +1,18 @@
 import * as Progress from "@radix-ui/react-progress";
+import { motion } from "motion/react";
 import { Download, ImageUp, Link2, RefreshCcw, X } from "lucide-react";
 import { Button } from "./ui/button";
 
 export function UploadWidgetUploadItem() {
   return (
-    <div className="p-3 rounded-lg flex flex-col gap-3 shadow-shape-content bg-white/2 relative overflow-hidden [&+div]:mt-2">
+    <motion.div
+      className="p-3 rounded-lg flex flex-col gap-3 shadow-shape-content bg-white/2 relative overflow-hidden [&+div]:mt-2"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{
+        duration: 0.4,
+      }}
+    >
       <div className="flex flex-col gap-1">
         <span className="text-xs font-medium flex items-center gap-1">
           <ImageUp className="size-3 text-zinc-300" strokeWidth={1.5} />
@@ -51,6 +59,6 @@ export function UploadWidgetUploadItem() {
           <span className="sr-only">Cancel upload</span>
         </Button>
       </div>
-    </div>
+    </motion.div>
   );
 }
