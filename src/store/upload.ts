4c6246d49da92516ffccb63a7fb58f128adebbea
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2026-02-10 01:03:05 -0300

    Utilizar biblioteca immer para facilitar alterar variáveis de estado sem precisar retornar o estado completo. Com suporte para Map e Set

--- src/store/upload.ts
+++ src/store/upload.ts
@@ -1,4 +1,8 @@
 import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
+import { enableMapSet } from "immer";
+
+enableMapSet();
 
 export type Upload = {
   name: string;
@@ -10,26 +14,22 @@ type UploadState = {
   addUploads: (files: File[]) => void;
 };
 
-export const useUploads = create<UploadState>((set, get) => {
-  function addUploads(files: File[]) {
-    for (const file of files) {
-      const uploadId = crypto.randomUUID();
-
-      const upload: Upload = {
-        name: file.name,
-        file,
-      };
-
-      set((state) => {
-        return {
-          uploads: state.uploads.set(uploadId, upload),
-        };
-      });
-    }
-  }
-
-  return {
+export const useUploads = create<UploadState>()(
+  immer((set) => ({
     uploads: new Map(),
-    addUploads,
-  };
-});
+    addUploads: (files: File[]) => {
+      for (const file of files) {
+        const uploadId = crypto.randomUUID();
+
+        const upload: Upload = {
+          name: file.name,
+          file,
+        };
+
+        set((state) => {
+          state.uploads.set(uploadId, upload);
+        });
+      }
+    },
+  })),
+);
