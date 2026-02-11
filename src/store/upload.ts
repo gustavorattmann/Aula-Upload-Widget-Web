f575f374acf7b86aee94856747b379083e2e62c7
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2026-02-11 12:06:38 -0300

    Adicionar monitoramento de progresso e corrigir ação de cancelamento

--- src/store/upload.ts
+++ src/store/upload.ts
@@ -2,6 +2,7 @@ import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
 import { enableMapSet } from "immer";
 import { uploadFileToStorage } from "../http/upload-file-to-storage";
+import { CanceledError } from "axios";
 
 enableMapSet();
 
@@ -10,6 +11,8 @@ export type Upload = {
   file: File;
   abortController: AbortController;
   status: "progress" | "success" | "error" | "canceled";
+  originalSizeInBytes: number;
+  uploadSizeInBytes: number;
 };
 
 type UploadState = {
@@ -33,14 +36,32 @@ export const useUploads = create<UploadState>()(
 
       try {
         await uploadFileToStorage(
-          { file: upload.file },
+          {
+            file: upload.file,
+            onProgress: (sizeInBytes) => {
+              set((state) => {
+                state.uploads.set(uploadId, {
+                  ...upload,
+                  uploadSizeInBytes: sizeInBytes,
+                });
+              });
+            },
+          },
           { signal: upload.abortController.signal },
         );
 
         set((state) => {
           state.uploads.set(uploadId, { ...upload, status: "success" });
         });
-      } catch {
+      } catch (err) {
+        if (err instanceof CanceledError) {
+          set((state) => {
+            state.uploads.set(uploadId, { ...upload, status: "canceled" });
+          });
+
+          return;
+        }
+
         set((state) => {
           state.uploads.set(uploadId, { ...upload, status: "error" });
         });
@@ -56,6 +77,8 @@ export const useUploads = create<UploadState>()(
           file,
           abortController,
           status: "progress",
+          originalSizeInBytes: file.size,
+          uploadSizeInBytes: 0,
         };
 
         set((state) => {
